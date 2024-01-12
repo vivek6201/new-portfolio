5ef23af2b15b3b4820e20faa9b775723f8b9df20
vivek6201 <gvicky4567888@gmail.com>
2024-01-13 01:53:01 +0530

    updated structure of the code base

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,6 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <Navbar />
           {children}
         </ThemeProvider>
       </body>
